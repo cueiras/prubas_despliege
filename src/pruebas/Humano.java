b7bfed8b9ab8e1f043ffe0c63d37fbdd046d9c2b
Miguel <cueiras98@gmail.com>
2018-12-10 09:01:21 +0100

    en rama de ficheros

--- src/pruebas/Humano.java
+++ src/pruebas/Humano.java
@@ -1,6 +1,22 @@
 package pruebas;
 
-public class Humano {
+import java.io.File;
 
+public class Humano {
+	
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	private void initTodo() {
+		File fichero = new File("hola.txt");
+	}
+	
 	
 }
