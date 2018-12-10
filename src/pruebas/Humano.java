4d9e28b69c28419a86531ed3fa3535543bdfeea4
Miguel <cueiras98@gmail.com>
2018-12-10 09:05:09 +0100

    leyendo datos

--- src/pruebas/Humano.java
+++ src/pruebas/Humano.java
@@ -1,6 +1,16 @@
 package pruebas;
 
+import java.util.Scanner;
+
 public class Humano {
 
-	
+	private void hola() {
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("hola");
+	}
 }
+
+
+
+
